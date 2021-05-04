09cadc32a7e7827e280318d0234f4a191ebd33d5
Victor Tran <trannvictor@gmail.com>
2021-05-04 01:37:36 -0700

    Tinkered ultrasonic sensor and servo motor on the nucleo_l476rg

---
+++ arm/stm32/nucleo_l476rg/ultrasonic_sensor/Core/Src/main.c
@@ -0,0 +1,490 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#ifdef __GNUC__
+	#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+	#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+
+UART_HandleTypeDef huart2;
+
+/* Definitions for ultrasonicSens */
+osThreadId_t ultrasonicSensHandle;
+const osThreadAttr_t ultrasonicSens_attributes = {
+  .name = "ultrasonicSens",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM1_Init(void);
+void StartUltrasonicSensTask(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#define TRIG_PIN GPIO_PIN_9
+#define TRIG_PORT GPIOA
+
+
+typedef enum
+{
+	GET_RISING_EDGE,
+	GET_FALLING_EDGE,
+} UltrasonicState_t;
+
+
+volatile uint32_t ulICValRise = 0UL;
+volatile uint32_t ulICValFall = 0UL;
+volatile uint32_t ulPulseWidth1ms = 0Ul;
+volatile UltrasonicState_t eUltrasonicState = GET_RISING_EDGE;
+volatile float fDistance = 0.0f;
+
+
+
+// Callback
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	// If the interrut source is from channel 1
+	if (htim->Instance == TIM1 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+	{
+		if (eUltrasonicState == GET_RISING_EDGE)
+		{
+			ulICValRise = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+
+			eUltrasonicState = GET_FALLING_EDGE;
+		}
+		else if (eUltrasonicState == GET_FALLING_EDGE)
+		{
+			ulICValFall = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+
+			// Reset the counter
+			__HAL_TIM_SET_COUNTER(htim, 0);
+
+			if (ulICValFall >= ulICValRise)
+			{
+				ulPulseWidth1ms = ulICValFall - ulICValRise;
+			}
+			else
+			{
+				ulPulseWidth1ms = (0xFFFF - ulICValRise) + ulICValFall;
+			}
+
+			fDistance = (float)ulPulseWidth1ms / 58.30875f;
+
+			eUltrasonicState = GET_RISING_EDGE;
+		}
+	}
+}
+
+
+void HCSR04_Read(void)
+{
+	HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);
+	osDelay(10);
+	HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);
+}
+
+
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+
+  // Start input capture in interrupt mode
+  HAL_TIM_IC_Start_IT(&htim1, TIM_CHANNEL_1);
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of ultrasonicSens */
+  ultrasonicSensHandle = osThreadNew(StartUltrasonicSensTask, NULL, &ultrasonicSens_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  for (;;)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the main internal regulator output voltage
+  */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_IC_InitTypeDef sConfigIC = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 80;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_IC_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_BOTHEDGE;
+  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+  sConfigIC.ICFilter = 0;
+  if (HAL_TIM_IC_ConfigChannel(&htim1, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+PUTCHAR_PROTOTYPE
+{
+	HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
+	return ch;
+}
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartUltrasonicSensTask */
+/**
+  * @brief  Function implementing the ultrasonicSens thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartUltrasonicSensTask */
+void StartUltrasonicSensTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    //HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_3);
+
+  	HCSR04_Read();
+  	osDelay(200);
+
+
+  	printf("Distance: %9.6f\r\n", fDistance);
+  	printf("Pulse: %lu\r\n", ulPulseWidth1ms);
+  	printf("IC1: %lu\r\n", ulICValRise);
+  	printf("IC2: %lu\r\n", ulICValFall);
+
+    osDelay(200);
+  }
+  /* USER CODE END 5 */
+}
+
+ /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- esp32/hello_freertos/main/main.c
+++ esp32/hello_freertos/main/main.c
@@ -1,7 +1,13 @@
+/*
+// Example x...
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+
+#include "freertos/timers.h"
+
 
 #define QUEUE_SIZE 3
 
@@ -91,3 +97,159 @@ void app_main(void)
         printf("ERROR: Queue cannot be created.\r\n");
     }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//// Example 13
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define mainONE_SHOT_TIMER_PERIOD (pdMS_TO_TICKS(3300))
+#define mainAUTO_RELOAD_TIMER_PERIOD (pdMS_TO_TICKS(500))
+
+volatile uint32_t ulCallCount = 0; 
+
+typedef struct
+{
+    TimerHandle_t xHandle;
+    BaseType_t xStarted;
+} Timer_t;
+
+
+static void prvOneShotTimerCallback(TimerHandle_t xTimer)
+{
+    TickType_t xTimeNow;
+    
+    xTimeNow = xTaskGetTickCount();
+    printf("One-shot timer callback executing at tick: %u.\r\n", xTimeNow);
+    ulCallCount++;
+}
+
+
+static void prvAutoReloadTimerCallback(TimerHandle_t xTimer)
+{
+    TickType_t xTimeNow;
+    xTimeNow = xTaskGetTickCount();
+    printf("Auto-reload timer callback executing at tick: %u.\r\n", xTimeNow);
+    ulCallCount++;
+}
+
+
+void app_main(void)
+{
+    Timer_t xOneShotTimer = 
+    {
+        .xHandle = xTimerCreate(
+            "OneShot",
+            mainONE_SHOT_TIMER_PERIOD,
+            pdFALSE,
+            0,
+            prvOneShotTimerCallback
+        ) 
+    };
+
+    Timer_t xAutoReloadTimer =
+    {
+        .xHandle = xTimerCreate(
+            "AutoReload",
+            mainAUTO_RELOAD_TIMER_PERIOD,
+            pdTRUE,
+            0,
+            prvAutoReloadTimerCallback
+        )
+    };
+
+    if ((xOneShotTimer.xHandle != NULL) && (xAutoReloadTimer.xHandle != NULL))
+    {
+        xOneShotTimer.xStarted = xTimerStart(xOneShotTimer.xHandle, 0);
+        xAutoReloadTimer.xStarted = xTimerStart(xAutoReloadTimer.xHandle, 0);
+    
+        if ((xOneShotTimer.xStarted == pdPASS) && (xAutoReloadTimer.xStarted == pdPASS))
+        {
+            printf("Successful start.\r\n");
+        }
+    }
+}
+*/
+
+//// Example 14
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define mainONE_SHOT_TIMER_PERIOD (pdMS_TO_TICKS(3333))
+#define mainAUTO_RELOAD_TIMER_PERIOD (pdMS_TO_TICKS(500))
+
+typedef struct
+{
+    TimerHandle_t xHandle;
+    BaseType_t xStarted;
+} Software_Timer_t;
+
+
+Software_Timer_t xOneShotTimer;
+Software_Timer_t xAutoReloadTimer;
+
+static void prvTimerCallback(TimerHandle_t xTimer)
+{
+    TickType_t xTimeNow;
+    uint32_t ulExecutionCount;
+    
+    xTimeNow = xTaskGetTickCount();
+
+    ulExecutionCount = (uint32_t)pvTimerGetTimerID(xTimer);
+    ulExecutionCount++;
+    vTimerSetTimerID(xTimer, (void *)ulExecutionCount);
+
+    if (xTimer == xOneShotTimer.xHandle)
+    {
+        printf("One-shot timer callback executed %u times at tick %u\r\n", ulExecutionCount, xTimeNow);
+    }
+    else if (xTimer == xAutoReloadTimer.xHandle)
+    {
+        printf("Auto-reload timer callback executed %u times at tick %u\r\n", ulExecutionCount, xTimeNow);
+
+        if (ulExecutionCount >= 5)
+        {
+            xTimerStop(xTimer, 0);
+            // if (xTimerStop variable pass/not pass...)
+        }
+    }
+}
+
+
+void app_main(void)
+{
+    printf("Initiate app_main.\r\n");
+
+    xOneShotTimer.xHandle = xTimerCreate("One Shot", mainONE_SHOT_TIMER_PERIOD, pdFALSE, 0, prvTimerCallback);
+    xAutoReloadTimer.xHandle = xTimerCreate("Auto Reload", mainAUTO_RELOAD_TIMER_PERIOD, pdTRUE, 0, prvTimerCallback);
+
+    if ((xOneShotTimer.xHandle != NULL) && (xAutoReloadTimer.xHandle != NULL))
+    {
+        xOneShotTimer.xStarted = xTimerStart(xOneShotTimer.xHandle, 0);
+        xAutoReloadTimer.xStarted = xTimerStart(xAutoReloadTimer.xHandle, 0);
+
+        if ((xOneShotTimer.xStarted == pdPASS) && (xAutoReloadTimer.xStarted == pdPASS))
+        {
+            printf("Successful start.\r\n");
+        }
+    }
+}
+
